0cbb64e2c90a6f2f83737087bdd80fb6b2be56e7
David <frodorik21@gmail.com>
2016-02-25 20:30:39 +0100

    creacion del script casi completo, con varias opciones incluidas

--- cleanfolders.sh
+++ cleanfolders.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 
 # ------------------------------------------------------------------
 # [Author] Title
@@ -8,8 +8,14 @@
 #
 # --- Options processing -------------------------------------------
 #path= /home/mishistorias
-DEST= "/home/myhouse/Music";
 
+function borrar() { 
+
+	for i in $(sudo find ${DEST} -type d -empty -exec {} \;) ; do
+
+		echo $i
+	done
+}
 
 # --- Locks -------------------------------------------------------
 #LOCK_FILE=/tmp/$SUBJECT.lock
@@ -20,11 +26,51 @@ DEST= "/home/myhouse/Music";
 # --- Body --------------------------------------------------------
 #  SCRIPT LOGIC GOES HERE
 
-if $(sudo find ${DEST} -type d -empty -exec echo '{}' \;) ; then
+echo "########Script para la limpieza de carpetas vacias##############"
 
-echo "el directorio contiene carpetas vacias" 
+echo
+
+echo "Seleccione las carpetas para escanear" 
+
+echo
+
+read -p 'Origen:' DEST 
+
+if [ -d  "$DEST" ]; then
+
+sudo find ${DEST} -type d -empty -exec {} \;
+
+echo
+
+echo
+
+echo  "Existen carpetas vacias"
+
+fi
+
+echo 
+
+echo
+
+echo "Quieres borrar las carpetas mencionadas????"
+
+echo
+
+echo "Contesta con si/no"
+
+echo
+
+read -p 'Que dices cerdo? ' RESPUESTA
+
+if ["$RESPUESTA" == 'si'];  then
+
+echo $DEST
+
+else
+
+exit 0
 
 fi
 
 
-# -----------------------------------------------------------------
+#-----------------------------------------------------------------
