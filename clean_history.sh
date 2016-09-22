b898d7a6318d708d52f2e2537f7e050bd7952483
dfar0 <frodorik21@gmail.com>
2016-09-22 07:30:34 +0200

    agrego script limpia temporales

---
+++ clean_history.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+# limpiar el historyco
+
+bleachbit -c "gnome.search_history google_chrome.cache google_chrome.cookies google_chrome.form_history google_toolbar.search_history google_chrome.history vlc.mru"
