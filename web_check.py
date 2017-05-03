534314450e6c138ec4c6916850434cdb788707c4
David Faro <david@rhel7t.ti.uam.es>
2017-05-03 16:46:01 +0200

    mejorando el chequeo de url

--- web_check.py
+++ web_check.py
@@ -5,12 +5,24 @@ import requests, sys
 
 #enlace = raw_input(sys.argv[1])
 
-url = raw_input("Que web quieres chequear?: ")
-response = requests.get(url)
-response.time = requests.get(url).elapsed.total_seconds
 
-print ("Respuesta de la web", response.status_code)
-print ("Tiempo de respuesta", response.time)
+url = raw_input("Que web quieres chequear?: ")
+segundos = input("Segundos del escaneo: ")
+segundos = int(segundos)
+
+def chequeo():
+    response = requests.get(url)
+    response.time = requests.get(url).elapsed.total_seconds()
+    print ("Web chequeada", url)
+    print ("Respuesta de la web", response.status_code)
+    print ("Tiempo de respuesta", response.time)
+
+while (segundos > 0):
+    chequeo()
+    print ('Tiempo restante del chequeo:', segundos)
+    segundos = segundos - 1
+
+print "Terminamos el escaneo"
 
 
 
