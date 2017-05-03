2c68a3113ea76b98ad954fcc3e94819f6c2717db
DavidF <frodorik21@gmail.com>
2017-05-03 17:09:59 +0200

    mejorando el chequeador

--- web_check.py
+++ web_check.py
@@ -1,26 +1,35 @@
 #!/usr/bin/python2.7
 # This Python file uses the following encoding: utf-8
+#Check Webpage By David F
 
 import requests, sys
 
-#enlace = raw_input(sys.argv[1])
-
 
 url = raw_input("Que web quieres chequear?: ")
-segundos = input("Segundos del escaneo: ")
+segundos = int(input("Segundos del escaneo: "))
 segundos = int(segundos)
+badtime = int(0.1)
 
-def chequeo():
-    response = requests.get(url)
-    response.time = requests.get(url).elapsed.total_seconds()
-    print ("Web chequeada", url)
-    print ("Respuesta de la web", response.status_code)
-    print ("Tiempo de respuesta", response.time)
-
+def chequeo(self):
+   response = requests.get(url)
+   response.time = requests.get(url).elapsed.total_seconds()
+   print ("Web chequeada", url)
+   print ("Respuesta de la web", response.status_code)
+   print ("Tiempo de respuesta", response.time)
+   
 while (segundos > 0):
     chequeo()
     print ('Tiempo restante del chequeo:', segundos)
     segundos = segundos - 1
+
+
+
+#if badtime => response.time
+#print ("Tiempo de respuesta alto")
+#else:
+#print ("Tiempo de respuesta optimo")
+
+
 
 print "Terminamos el escaneo"
 
