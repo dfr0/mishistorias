1561856607632321b5dabb47e5288a514c04841a
DavidF <frodorik21@gmail.com>
2017-05-04 09:04:41 +0200

    cambios en chequeo web

--- web_check.py
+++ web_check.py
@@ -10,24 +10,26 @@ segundos = int(input("Segundos del escaneo: "))
 segundos = int(segundos)
 badtime = int(0.1)
 
-def chequeo(self):
-   response = requests.get(url)
-   response.time = requests.get(url).elapsed.total_seconds()
-   print ("Web chequeada", url)
-   print ("Respuesta de la web", response.status_code)
-   print ("Tiempo de respuesta", response.time)
-   
-while (segundos > 0):
-    chequeo()
+def chequeo():
+    response = requests.get(url)
+    response.time = requests.get(url).elapsed.total_seconds()
+    print ("Web chequeada", url)
+    print ("Respuesta de la web", response.status_code)
+    print ("Tiempo de respuesta", response.time)
+
+def contador():   
+    while (segundos > 0):
+    return chequeo
     print ('Tiempo restante del chequeo:', segundos)
     segundos = segundos - 1
 
 
+#print ("Respuesta de la web", response.status_code)
+#	if response.status_code => badtime:
+#print ("malos tiempos de respuesta")
+#	else:
+#print ("Buenos tiempos de respuesta")
 
-#if badtime => response.time
-#print ("Tiempo de respuesta alto")
-#else:
-#print ("Tiempo de respuesta optimo")
 
 
 
