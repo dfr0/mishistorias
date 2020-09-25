a34bf152be380f9d1b6358a6f90835bbab6bfce6
qk82aj <david.faro.chaparro.ext@ing.com>
2020-09-25 12:52:14 +0200

    (dev) sentencia if

---
+++ c++/3_estructuras_condicionales/sentencia_if.cc
@@ -0,0 +1,33 @@
+/* la sentencia if
+ 
+
+   if(condicion){
+   	intrucciones 1;
+	}
+   else{
+        instrucciones 2;
+   }
+
+  */
+
+
+#include<iostream>
+
+using namespace std;
+
+
+int main(){
+	int numero, dato = 5;
+
+	cout<<"Digite un numero: ";
+	cin>>numero;
+
+	if(numero == dato){ //== es el operador de igualdad no de operacion
+		cout<<"El numero es 5";
+	}
+	else {
+		cout<<"El numero es diferente de 5";
+	}
+
+	return 0;
+}
