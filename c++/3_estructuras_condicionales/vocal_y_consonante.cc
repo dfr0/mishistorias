cea0bc3a73d7812d5d958bd3689a489e24cf2ad1
Davidf <david.far.chaparro@gmail.com>
2020-09-30 14:50:01 +0200

    (dev) added vocal exercice

---
+++ c++/3_estructuras_condicionales/vocal_y_consonante.cc
@@ -0,0 +1,13 @@
+//Escriba un programa que lea la entrada estandar un caracter e indique en la salida standar si es una vocal miniscula o no_wq
+//
+//
+#include <iostream>
+
+using namespace std;
+
+int main() {
+	char letra;
+
+	cout>>"\n Digite un letra";cin>>letra;
+
+}
