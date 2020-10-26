0d1f4cac4930105cbceb813f40da2099d7ed1769
qk82aj <david.faro.chaparro.ext@ing.com>
2020-10-26 10:11:22 +0100

    (dev) determinar suma elementos

---
+++ c++/5_vectores/determinar_valor_suma_elementos.cc
@@ -0,0 +1,10 @@
+//Escribe un programa qye defina un vector de numeros y calcule si existe algún numero en el vector cuyo valor equivale a la suma del resto de numeros del vector.
+//
+#include<iostream>
+#include<ncurses.n>
+
+
+using namespace std;
+
+
+
