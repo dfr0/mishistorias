f42e452398350cd0acdd1b87f0941b4e4e422ef9
qk82aj <david.faro.chaparro.ext@ing.com>
2021-02-22 15:14:29 +0100

    ejercicios de cadenas

---
+++ c++/7_strings/ejercicios_cadenas.cc
@@ -0,0 +1,24 @@
+//crear una cadena que tenga la siguiente frase "hola que tal". luego crear otra cadena  para preguntarle al usuario su nombre
+//por ultimo alañir el nombre al final de la primera cadena y mostrar el mensaje completo "hola que tal (nombre del usuario)
+//
+
+#include <iostream>
+#include <string.h>
+
+using namespace std;
+
+int main(){
+
+	char cabecera[]="el nombre de usuario es ";
+	char titulo[50];
+	char usuario[30];
+
+	cout<<"Digite su nombre de usuario "<<endl;cin>>usuario;
+
+	strcpy(titulo,cabecera);
+
+	strcat(titulo,usuario);
+
+	cout<<titulo<<endl;
+
+}
