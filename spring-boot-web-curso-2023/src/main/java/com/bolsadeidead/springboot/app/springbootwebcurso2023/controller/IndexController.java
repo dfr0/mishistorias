4ac0356a11697184f851b96c759975edf1bb19d0
QK82AJ <david.faro.chaparro.ext@ing.com>
2023-05-23 18:26:21 +0200

    starting

--- spring-boot-web-curso-2023/src/main/java/com/bolsadeidead/springboot/app/springbootwebcurso2023/controller/IndexController.java
+++ spring-boot-web-curso-2023/src/main/java/com/bolsadeidead/springboot/app/springbootwebcurso2023/controller/IndexController.java
@@ -1,4 +1,18 @@
 package com.bolsadeidead.springboot.app.springbootwebcurso2023.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller //componente de spring tipo controlador
 public class IndexController {
+
+    //para mapear
+    @RequestMapping(value="/index") //por defecto GET
+
+    //metodo
+    public Static index(){
+
+        return "index"; //se mete en estaticos
+
+    }
 }
