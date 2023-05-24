c44d15a427e3b65c8da7a27243259782a63aaa26
QK82AJ <david.faro.chaparro.ext@ing.com>
2023-05-24 19:57:49 +0200

    added first level route

--- spring-boot-web-curso-2023/src/main/java/com/bolsadeidead/springboot/app/springbootwebcurso2023/controller/IndexController.java
+++ spring-boot-web-curso-2023/src/main/java/com/bolsadeidead/springboot/app/springbootwebcurso2023/controller/IndexController.java
@@ -2,20 +2,30 @@ package com.bolsadeidead.springboot.app.springbootwebcurso2023.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.util.Map;
+
 @Controller //componente de spring tipo controlador
+@RequestMapping("/app") //ruta de primer nivel, metodos handler rutas de segundo nivel.
 public class IndexController {
 
     //para mapear
     //por defecto GET podemos usar GetMapping también etc..o Post
     //el value= se puede omitir
-    @RequestMapping({"/index","/","/home"})
-
+    @RequestMapping({"/index","/","/home"}) //como tenemos / poner al final del primer nivel /app
     //metodo
     public String index(Model model) {
         model.addAttribute("titulo,Hola spring framework");
         return "index"; //se mete la pagina en src estaticos
 
     }
+
+    /*podemos usar el map de utils para lo mismo, también puede usarse modelandview
+    @GetMapping("/index2")
+    public String index2(Map<String, Object> map) {
+        map.put("titulo,Hola spring framework con MAP");
+        return "index"; //se mete la pagina en src estaticos
+*/
 }
