c1a0561ce93b8cc64a32e58efacfe88f80ea3a23
QK82AJ <david.faro.chaparro.ext@ing.com>
2023-07-22 22:34:59 +0200

    added new examples paramcontroller

--- spring-boot-web-curso-2023/src/main/java/com/bolsadeidead/springboot/app/springbootwebcurso2023/controller/EjemploParamsController.java
+++ spring-boot-web-curso-2023/src/main/java/com/bolsadeidead/springboot/app/springbootwebcurso2023/controller/EjemploParamsController.java
@@ -2,14 +2,24 @@ package com.bolsadeidead.springboot.app.springbootwebcurso2023.controller;
 
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 @RequestMapping("/params")
 public class EjemploParamsController {
+
+    @GetMapping("/")
+    public String index(){
+        return "params/index";
+    }
+
+
         @GetMapping("/string")
-    public String param(String model) {
-            return ("params/ver");
+    public String param(@RequestParam(required=false,defaultValue = "algun valor") String texto, Model model) { //todos los paramtros se llaman por nombre
+            model.addAttribute("resultado","El texto enviado es: " + texto);
+            return "params/ver"; //sirve para pasar parametros en la url
         }
 }
